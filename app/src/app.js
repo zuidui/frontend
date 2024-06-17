7ba7bfbd0fc59d61d742c96d1b608b25c2ac7cad
manu <manloralm@outlook.com>
2024-06-17 13:10:14 -0600

    Fix js

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     const env = await loadEnvVariables();
     const apiGatewayHost = os.GetEnv('API_GATEWAY_HOST') || env.API_GATEWAY_HOST;
     const apiGatewayPort = os.GetEnv('API_GATEWAY_PORT') || env.API_GATEWAY_PORT;
-    const apiGatewayUrl = `http://${apiGatewayHost}:${apiGatewayPort}/v1`;
+    const apiGatewayUrl = `http://${apiGatewayHost}:${apiGatewayPort}/api/v1`;
     console.log('API Gateway URL:', apiGatewayUrl);
     console.log('Environment variables:', env);
 
